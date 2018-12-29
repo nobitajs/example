84e8642bed72ee62cb1ba50209dde1799988943b
JJ <tangjun@zuzuche.com>
2018-12-29 16:57:46 +0800

    v0.5.0

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-  async index (ctx) {
+  async index () {
+    const ctx = this;
     let res = await ctx.service.main.getName();
     ctx.body = ctx.nunjucks.render('index.html', res.data);
   }
