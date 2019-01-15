ed6eba6eef7849f4b06acfb8677df4603afa8ac0
JJ <tangjun@zuzuche.com>
2019-01-15 15:44:50 +0800

    v0.6.7

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,6 @@
 module.exports = {
   async index() {
     const ctx = this;
-    ctx.logger.error('333');
-    console.log(555);
     ctx.body = ctx.nunjucks.render('index.html', { name: 'Hello Nobita' });
   }
 };
