e659196017ddad6f298956e93ecbd9c73bafd558
JJ <tangjun@zuzuche.com>
2021-01-15 10:23:25 +0800

    add

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,67 @@
+const axios = require('axios');
+const download = require('download');
+const u = require('../../u');
+
+const urls = [
+  
+]
 
 module.exports = (app) => {
   return {
     async index() {
       const { ctx } = this;
-      const data = ctx.service.main.getName();
-      ctx.body = ctx.nunjucks.render('./index.html', data);
+      for(const url of urls){
+        await axios({
+          url: `http://www.qiman6.com/${url}`,
+          method: 'get',
+        }).then(async (res) => {
+          const title = res.data.split('<h1 class="chaptername_title">')[1].split('</h1>')[0];
+          const text = res.data.split('eval(')[1].split(')\n')[0];
+          eval('global.func = ' + text);
+          eval(global.func);
+
+          let i = 1;
+          let p = [];
+          for(let src of newImgs){
+            p.push(download(src, `../鬼灭之刃/${title}`, {
+              filename: `${i}.jpg`.toString()
+            }).then(() => {console.log(`鬼灭之刃/${title}`, '下载完成')}).catch(e=>{}))
+            i++;
+          }
+
+          await Promise.all(p).then()
+          console.log(`${title}:完成`);
+          console.log(url)
+        })
+      }
+      console.log('鬼灭之刃:全部完成')
+      ctx.body = ''
     },
 
+    async other() {
+      const { ctx } = this;
+      let i = 31;
+      while(i <= 182){
+        let j = 1;
+        const url = `https://twocomic.com/view/comic_10406.html?ch=${i}-${j}`;
+        const {page} = u(j, url);
+        const p = [];
+        console.log('共',page, '页')
+        while(j <= page){
+          const url = `https://twocomic.com/view/comic_10406.html?ch=${i}-${j}`;
+          const {img} = u(j, url);
+          let page = j;
+          p.push(download(img, `../一拳超人/第${i}话`, {
+            filename: `${j}.jpg`.toString()
+          }).then(() => {console.log(`一拳超人/第${i}话-第${page}页`, '下载完成')}).catch(e=>{}))
+          j++;
+        }
+
+        await Promise.all(p).then()
+        i++;
+      }
+      
+      ctx.body = ''
+    }
   };
 };
---
+++ u.js
@@ -0,0 +1,81 @@
+
+var p = 1;
+var chs=182;
+var f = 50;
+var ti = 10406;
+var cs = 'jbx1x13x15jghpabcp4jxkvepd68rsqghvsg4ccc9me9y4cn5ajbx2x13x16qph6f2wnncjg84vsdpm39uxckg6s3xggfcqqg8u9jbx3x13x20sgbjuehndgh3u6trx99qabp9usr5yrbbe9epeckhjbx4x13x208xacgtap4ja5hkwqarbkep5rpc4dbe4dtknbke4vjbx5x13x199sbpf4xwjb5dby7djkfxx94qr2f7vdxs8bgwdf36jbx6x13x24epee7m53myejtj658fgj747p6h5hryb2tes9ueypjbx7x13x17vgxsrrd6ymkb25d7784cf6pjnbt8sr488k846emhjbx8x13x22xm9g8cpnbex93rke92fyxjgrmwfuuu7a52wrhs4cjbx9x13x24b99pmfpa3fcf7ervhqtseguxwm5qd86744r7e5ahbx10x13x28a7mgxevqvp76a7qygk5tbbpwuhkqetdyayx267pvbx11x13x22ana2uru8gh58w37ahsf8ek683tce4grqefe2pnqsbx12x13x24r46g9y33wedyk3f3v4x9sn9vkh9d25anhwp34mwnbx13x13x25vkdh7s9auj4cshu9m4y7hdutpx8xmcmv7x2tkuuebx14x13x225uq9daw65qfssgbkanwrkc9duhysa6tb32njww9wbx15x13x266y79363cwq4truuu629jkgfph9bwf354s7ewkjdmbx16x13x235jsv3m8pkv7d7k9cph4skub3ad6bkfhdvabh4vq3bx17x13x316pwg2x4et3ny9dgb386thu4mdcuymb3cmu6492ghbx18x13x13uq46v6mr3qqn6epnw7kxfmrmhvd8gpf23bjvu8w4bx19x13x18qg69rgjnj58d7hfktahcu2sdg78me7ksbs338xunbx20x13x27dksy522wgau4hx4dgjgyj5qaqqmgp3cyvjw66ygubx21x13x17fqa5s3rj9b3dfc729adb66msgtcd4sysqrfhjxsfbx22x13x23m4nb6hnjt3jdddspj7ppkp6rvshwfvmtnk7rawhkbx23x13x2552uqtr4cvgbqcqj6we2k6puuqchncxth6m975yv4bx24x13x16vamtc4d4khb6w3tapj8u5j9g679826u59dw3qsmkbx25x13x23urdjt7npjvgwwteykt2xhhwctmcq4u4cnw5w3vgvbx26x13x46gmgyw7wfsgn8sfag2bxuak6m83njbe68nmqsa3fhbx27x87x37dxvbtuv582asbjp367u28pbgnte87y3tathnt23abx28x87x17ah7tveyx3425682p42m96mpnqef49bck62cwh7bmbx29x87bx8ads4epa7qe4vvb8n8qv6fb2x2nd46mjsyy5bk8y3bx30x87x53rgnu437u3qkm95whc2cg8b7crjnqy4ggkrgmrfpxbx31x87x27dwsb2pq8t7hu8b8jtcxhg6pdxca2qucxqpt8xxd8bx32x87x12j2j99w6ywrvbjvs4ssccc4a3a6ehpuyahbn7d6ybbx33x87x14ybseyqew5e3fq64euvh7br2xwfqfxk6772rpqpadbx34x87x26wh99mhcqbj7x3q7frqgxsbhy2e3ggpbv7kvb5gurbx35x87x25v99c4qfdyu5nu2qyvyws7qv738afykhjsyguedvvbx36x87x192c7kg8qtkmr84akkpy9sheww464h3ddkdcdh7v6wbx37x87x20mahkd6wj7x3xd4y8bgutr43mc4us854q2pkxsjaabx38x43x35gn6v4qnj644rur2ww3wmngvbnswv57rrpymqwdf7bx39x87x126hxkxhctsmwhenwka79nr3ywpprqcpu7xs9fb5wybx40x87x167qqdnx83qqbe4mt5ht3xr3mex7aru48xprhjfw7sbx41x87x23qbt9d7qja44ymtqr42hjm3yv9uye2s9pvhcuvh8hbx42x87x317ecaempxdwcbjqkeftcdw4whgfchkaw38k94ba26bx43x13x20dj2rce94rcwryqmn8kbc5mmuh2pywv7ppggejwwhbx44x23x19eykue27594f2ucgd8xy2kmnfa4g644vpsnj28caubx45x13x24qaaccr6426yjmry7abtvas5vxjxybk52r2kuwf6tbx46x13x15w7mkv8ujfddeym55qsvnk8eqptmw6fufchm4t4qsbx47x13x36ujk3ghbn5e5rfc8n43gfqbntmvkgqehmtsu9sg48bx48x13x19yapxv3v5jqc4p2aeh9w72jfpsnphf8x5cd4ph67hbx49x43x20s7muvfqe27skvgu96c6mgh25cuwewvjxvjkahb5pbx50x13x14pfuhn84ym2emtjub43a9rwkjwucmmw5qqu9d8b27bx51x13x137bmmguj6m6qy7pf8ybwhtfqvtf26rv5thmctdbxnbx52x23bx92uhc9tkkqk877hgps6kn4r98pvbktmhe5me9rr95bx53x43x237b3bs7b4sxgbnc3udumndgjudnh2kk98uecfja7ubx54x23x216dkgqcdufaj62rhayd9ajeb67wpp6u2bnhyrahrcbx55x87x14qff2r5sg4bwjkjhctuce28j4gsxd8n6na8h27t7hbx56x87bx92qenr3n9h86ub74s8kfxt24cd48h99ww2hb3e2khbx57x13bx834vg9ggnuvss7q8vqagcax5fg5sac5ev5am85k2abx58x23x15qpvp5pg372f5xtbw8xscc7jpc9mxg9rveuq3ajpabx59x23x20hm2gm6wnm7arh64m28c8rk9c6x37t528484w6aw7bx60x13x26tdxnv2qc2wax58dupu69k994r45r9wns934qd8y9bx61x13x14fa3tkxd75ky83gc53x76y2cwea5uanysf9gesnujbx62x13x115k89p2ynmpm75hxp3se3y6w3x5ug6w9hqgaqpey5bx63x13bx9pmgwamgwmqkft2suvxmbnstxfdqb7k5xpgxd56u9bx64x23bx8c2xdcdhqpq57r4rrs7eck6vjtmfccu7vkcd7faq6bx65x13bx59htqg2n3en695bhbba5cpbkrejkkya42b2yjtdqcbx66x23x1123unqt8eyb5hucu567sp6va78hgr8nb83mmx9a4wbx67x43x22rjtr6kk6y89fggjcyjf26grad8ptd6eeastx2ptdbx68x87x14hggbfjda45ge7rgqx7dj6su8k4dru3pnue5hkww6bx69x87bx9nmpmt43rnnpvtg26bc56jry79jbv6ansj6wwywe2bx70x23x12q4cjb39jptgdhssgghc62gr794jumj7st6yry4nabx71x23x12n7ew2xxs8sc6qedt2b2jdsmd2yb4mtvsxgqp9j7tbx72x23bx83r38a5ejh39g5n9e4v4dujsgdhrr8h3x7k4yb7webx73x23x15h5kh599wnrdkekxp3u29nnw9wk8yke45ts8mmm77bx74x23x16p3y96qgpp6wgdbsv3a88a6cseacd3csba8y26cg6bx75x23bx9eygddjvy83vk5c3rnt77g4ep38xpru5hua2h8sxkbx76x23bx442v3nde3rvnjv8puj5uhbkt7cscu7xhs3xw8d5mvbx77x87bx8phjhwmud2u5g7xyb6kf3n3d9jdya8dvqjmvpgaupbx78x23x11nc2j662da4qqksj5sscwbs4cvd6dptkhescbkqpqbx79x13x114rf2pc42jrenfgrdk56vwvtb2grqkpqtxxm7qgx5bx80x87x14s3fwqx9ye4v47ntju63erc76973d88mwh7bnx7jmbx81x87bx6rfd4f8xdg2k2vwt2y4nts4xj3awt8h2ajx8pw9v5bx82x87bx7vugrhea5cb6v77t24dpupcbtmejem3tqnkxuwe9vbx83x13x14pn85cak69e6vnxbhh27k64vkguk9xnpvpr3hrxdvxl84l13l13cpx8ar7mnttd645n5ytke8xvvcr9u2dpj3wxhmn4xl85l23l10gxbmgsvnfe5nebk5nkp63ms79prjgna48w8f4cn3xl86l23l13vf76jxwtt4w23bsnvw6ma697dmn673s2p9rhfg6wxl87l23xl8aatad6d3crd7mhtk93e32xaeeaw922ncygmcv6ddxl88l87l12yq7ew2wta6h4ggqr3wm2kg7e2t2ejakgdp89dr2kxl89l13l14qf7uk5jb8wguxy2v2b7weqtgfcp8trps44ung6egxl90l13l26qnrjvaj75juxc5bse4fhpd54u8hyhetjqhdqeha6xl91l23xl7n47fx9t23wkm7y2f8ffdy7643dcec9rn3tmep7cgxl92l13xl97mtsvcc5d9kfjygh69jcxrgdrew85mntfgu879jkxl93l87l1266kjgv3qudx772wv2cxdv2kadd8xy34cbeckmjjgxl94l23xl9bsd9xxf4m6c2b4473pyfh9qa2s4w6y3brwdf89pexl95l87l1437pb2bjhcw57dccs3abg7whsq62ehdvkstgqrhnpxl96l13l145nkswyfe2kt5736tfy29d289x78qmpj8yy2wrgbwxl97l87l23dnp5y7kxm2qapnw4xkbd7kvbe2epmva8m7ttk568xl98l13l14crwefgt8muregh4ey9m92x3wtq93ee8uxwsbky8gxl99l87l22vb777gbp7vntk64xwg5qs6xehm9r742sub3d6p7tl100l13l12cune4vqn5h7bbhymdkwuhrsv9fgqmgp548bqju5pl101l13l12ktcvs8fnqx7jqqm4pru4bmurpasnhvcwdctggd8dl102l87l258ddw8ypk35dq6bkm56jkcm6qq6rka6fgj43jgujyl103l13l30ha5e6r7qb86gt7aq7tujkqbbu45ywdmujwkb4mxkl104l13l156epr3eseyfas5r887adamnctfc5wf35ec7xy6s96l105l87l25s9b486emj6mxf2khvmrhnpbpjvkskrh5p4fuy2ndl106l87l41ffxjjpxn6yyh3h8x3742j9ggmtr5pchhdw6yycktl107l87l20jxw6vx9x2vsg9u3wxbfp3pw42rmesu9jdv9j4hmwl108l87l32ajjjw2w4k897jsqbnqwdcv28cdbmh9k9kpyhhdqpl109l87l422q3n69fcae9n7akpes7aw3j6j99vdh5u6b7xqfkdl110l43l25t58xe4djswpxtt85u6q3bu25e5h5v34k4x5wheprl111l13l34dfce4hcjjjfam2qrxdu8p747rdvbx5y8rw7rhj6tl112l13l33rt55khnc5sgfgnab24r3m7aggw575ud3xjde4vrhl113l13l11qcww8bn234mupnhhhf7u6g45yfwcwxpqpnffpcexl114l13l19kncwp2hv98kp3qfbk9yhpfsha2tyb4j2m3mbjvddl115l87l23hbmab38mhgyxxneqdy57ysp7mhjv4sghwbuttxypl116l13l4057gsr2ahugubv86euqg5d7hbasyvhdd3qcta6kswl117l87l17h4u5te2xjpsq5ubktrk2sv6ygq8qr9j78nmrwk55l118l13l31rv2tbadcs438jt43x8ge425d8e99x2rttkcdwk98l119l87l41h8xg67xx6udd489dqbu5v7uqycvvtt67xd5dck74l120l87l472svqc8ffkwuhmh4pvse2xsukq6tb852gmd43avnyl121l87l334pvweetwnyjhq3bhnh3ef9sxs2b7vxptq8m4hycnl122l13l49kxtxt6ms9xv32yb346xbvutudmbrgemq9na4rux5l123l87l58jy9jn5grpaunayqvu3hrj2ha6293eqxevschugdtl124l13l56j6tuhq8stbpjbvtb66ks7sgg4v876jtpypw82ge9l125l13131g7b7exs5g4bs3ppae6g7v3ndam7m6vk6bc23vqdvl126l13l44ggysdhqufg6j2tu73xtrtu68yjajm3c945cfhm9wl127l13l317kkce6y3jh3gm7mjge5hccsm26xbf55tct8c5y2nl128l13l43xhuappkjtpbpr4em9wk82cuqq39y6tn7r2y4jgc5l129l87l58a3je27mvcc3mn45n8tqeakt5t56gec2b7vb549bql130l13l99yqvg684td9kpku3nk6jpeftav9kmy3sxq23rkh48l131l13l46gq4a6ewwg99je4y26xk48dhqqcnm3x6tmjem7afdl132l13l59au4ykyxewswdfkuy5n926kymhuf664qew5a86pfal133l87l66pwu2g73uhjn342k75xkwky5pfhuak9e87rexjcngl134l87l70mkxu477d3w843hqndj8n9trdxp32hwgbf8fmq3e9l135l871429v29x6nb36vd9axyeetqb8mvv9hqh5aejbyxk2nml136l43l53p2k44kees83bhncefwghjbp45pdf5pg6y9y5vwt6l137l43l31tdar68rnx6wxjhx4schexb687x8gaus4rd8a2n48l138l43l32e95trhb3pmn8cvy3pctas8pfdyapud5ef5d8xxyjl139l43l38s5vjnc8emj34pqqqxax6464fnk98cqv8r4haw3q7l140l43l37b67cr49rvg7334v7cta72anthfegn8bw8q7sfv3xl141l43l484jwp25b8udtd89mrsb44ncudspvyrxuvxmqehchcl142l43l2724nsex6429hjkax2v78p5uxbamb4cpuyjnqs7pb3l143l43l47pqa6aj7f2g79qs3aj3cegpgdu4rekk76gdhhhsghl144l87l14mjedjak8ppey8gyhtadr6sa8efu557fsdmjy7shel145l43l13p624r4gw7bje3gstugn4cbradv8wfrdcav4cjug9l146l43l216y9cbpanea4efk9365889nn86xwwfdu3nystcyt5l147l43l39njafpabtg9fuf8nmx3cn2pm7nhrr5ufurr4jt8ybl148l43l35utq72q9xef6h7m2f9wkht2e62ywqnva8sxx4fch5l149l87l192yyqbds88thqhpd4g26gxg63w8nx5c2unvwbmm5pl150l87l35dydj26w86et3evuetyev3q763vyjhcbrp8xp8e3yl151l43l18ch5ejphb64py7raqesndt3vtgkcdh5yt57j2m9q3l152l43l26t4bfn5btkx6jrhhdkbcbwkbhpmetneumfjgah93ql153l43l245fj2vvt4h6m8sm9794pyw7aqjnwkmytaumrrpwy4l154l87l26h48yg4gk83kqtjsdtvqcf26xqxe5bt9g5qn2y4apl155l43l263b3ju3xkbp3tenhetetyr6twrvc5vgcc48mfkbf6l156l43l21h5b632w3ds4s2g5x8r2nuratfjhucw4xfmmvwvsgl157l87l27pbewxvjgqk6262vupkb8m52b55yjbnmrsx35vgycl158l87l22q8u4cej98fg6ssxu7g2mvssyv7mkhjq8ndfaehe9l159l87l19ybx6jm7qtu2jaxmfn3cpwtgatjac8ab8jaqc6ydgl160l87l26pp4m2yqmkd7h95wbyfh47yrpv56mt3tysx54mf45l161l87l288twqd7rs6axjrettngqecx4v7t3rq5ypj436ynyxl162l43l2483t4a339tbyfx5wvyymaem5xxdvjv346ptrve7apl163l87l20phuke9c82fgfk36fhcmmtteudkqhydqnj98ffjxdl164l12l21y52mawg3ajmfvj6j7894ktp7fa89waq37bnvv288l165l83l255u6dgarhqvmcyjkcd3ndjuhjntmucac6r83epvhkl166l83l16u4duswba9rhhbbd5m3bkhqr9mntdt4hdqak76jxcl167l83l133grgxeydjhw24b2n3qqxnfgg2ug2qj597u5v93ecl168l83l192ywanmrhhkcujjfpc8p9m9dgvkwrfbdy5spbqcpcl169l83l36b6y9jt2tg7jn2ba5wakck7ur53y6hnws2933uqxtl170l83l337kvm5man7cqeu523a7yd5a477yhhk8t5jkh9w479l171l83l24eju6c7wtfrs2j2fd34rpc5uatgkbc5267cakkh6wl172l83l26kshxjcf6496chfxuywu7npuwutrf9hvgjmmprw8ml173l83l24dpujb6ybha9hhd8vs6hqy8qhymd9eqtfqyxf773cl174l83l23ade4hrpuc75xa377rf8xrt8w2da6jf4wjtj4jy9jl175l83l21yvs3deqqxspb9gx4c5vpr4udeb9738kwckpp853yl176l83l417hqmswgn68sa73tw6g7at6xuwrruy36gs87gbdntl177l83l28utvems9884vj9hptm55fafyu36vadqdcysfh4na3l178l83l35buk443fhpsejj3wuesjvpxtwvvny4yjnnmf2ff43l179l83l12w8bfxhgxmykqe5wygsbpwa3adnkv5buh7vrujtv2l180l83l34mswnuegq8u8vu9nsnuacj9y4nkaxykae78bb4974l181l83l34ehbe8tjuv22urdeb2dtxjp26hfnn739gtqh7yrptl182l83l392jfam3w2qsh9cbrwa6rj3tg7eh5pxgg2gymn8hxj'
+function request(queryStringName, URLString) {
+    var returnValue = "";
+    // var URLString = new String('');
+    var serachLocation = -1;
+    var queryStringLength = queryStringName.length;
+    do {
+        serachLocation = URLString.indexOf(queryStringName + "\=");
+        if (serachLocation != -1) {
+            if ((URLString.charAt(serachLocation - 1) == '?') || (URLString.charAt(serachLocation - 1) == '&')) {
+                URLString = URLString.substr(serachLocation);
+                break;
+            }
+            URLString = URLString.substr(serachLocation + queryStringLength + 1);
+        }
+    } while (serachLocation != -1)if (serachLocation != -1) {
+        var seperatorLocation = URLString.indexOf("&");
+        if (seperatorLocation == -1) {
+            returnValue = URLString.substr(queryStringLength + 1);
+        } else {
+            returnValue = URLString.substring(queryStringLength + 1, seperatorLocation);
+        }
+    }
+    return returnValue;
+}
+
+
+function ss(a, b, c, d) {
+    // console.log(a, b, c, d, '====e======')
+
+    var e = a.substring(b, b + c);
+    return d == null ? e.replace(/[a-z]*/gi, "") : e;
+}
+function si(c, p) {
+	// console.log(p)
+	console.log('https://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(p) + '_' + ss(c, mm(p) + 10, 3, f) + '.jpg')
+    return 'https://img' + ss(c, 4, 2) + '.8comic.com/' + ss(c, 6, 1) + '/' + ti + '/' + ss(c, 0, 4) + '/' + nn(p) + '_' + ss(c, mm(p) + 10, 3, f) + '.jpg';
+}
+function nn(n) {
+    return n < 10 ? '00' + n : n < 100 ? '0' + n : n;
+}
+function mm(p) {
+    // console.log(p, (parseInt((p - 1) / 10) % 10) + (((p - 1) % 10) * 3), '=====p======')
+    return (parseInt((p - 1) / 10) % 10) + (((p - 1) % 10) * 3)
+}
+;function sp(p, URLString) {
+    var ch = request('ch', URLString);
+if (ch.indexOf('-') > 0) {
+    p = parseInt(ch.split('-')[1]);
+    ch = ch.split('-')[0];
+}
+if (ch == '')
+    ch = 1;
+else
+    ch = parseInt(ch);
+    var c = '';
+    var ci = 0;
+    var cc = cs.length;
+    for (var i = 0; i < cc / f; i++) {
+        if (ss(cs, i * f, 4) == ch) {
+            c = ss(cs, i * f, f, f);
+            ci = i;
+            break;
+        }
+    }
+    if (c == '') {
+        c = ss(cs, cc - f, f);
+        ch = chs;
+    }
+    const ps = ss(c, 7, 3);
+    return {img: si(c, p), page: ps};
+}
+
+
+
+module.exports = sp;
