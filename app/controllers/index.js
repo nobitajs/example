1685987abca77c8b3c9e96d7333690be0428c504
JJ <tangjun@zuzuche.com>
2021-01-15 17:48:28 +0800

    add

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,7 +3,57 @@ const download = require('download');
 const u = require('../../u');
 
 const urls = [
-  
+"/16080/691266.html",
+"/16080/635783.html",
+"/16080/566485.html",
+"/16080/547631.html",
+"/16080/547626.html",
+"/16080/547623.html",
+"/16080/547608.html",
+"/16080/547593.html",
+"/16080/547574.html",
+"/16080/547568.html",
+"/16080/547564.html",
+"/16080/547559.html",
+"/16080/547540.html",
+"/16080/547535.html",
+"/16080/547534.html",
+"/16080/547525.html",
+"/16080/547519.html",
+"/16080/547515.html",
+"/16080/547503.html",
+"/16080/547475.html",
+"/16080/547474.html",
+"/16080/547468.html",
+"/16080/547460.html",
+"/16080/547436.html",
+"/16080/547430.html",
+"/16080/547423.html",
+"/16080/547402.html",
+"/16080/547401.html",
+"/16080/547394.html",
+"/16080/547390.html",
+"/16080/547378.html",
+"/16080/547372.html",
+"/16080/547367.html",
+"/16080/547358.html",
+"/16080/547353.html",
+"/16080/547351.html",
+"/16080/547334.html",
+"/16080/547327.html",
+"/16080/547319.html",
+"/16080/547291.html",
+"/16080/547286.html",
+"/16080/547276.html",
+"/16080/547275.html",
+"/16080/547271.html",
+"/16080/547258.html",
+"/16080/547252.html",
+"/16080/547234.html",
+"/16080/547230.html",
+"/16080/547222.html",
+"/16080/547221.html",
+"/16080/547213.html",
 ]
 
 module.exports = (app) => {
@@ -23,9 +73,9 @@ module.exports = (app) => {
           let i = 1;
           let p = [];
           for(let src of newImgs){
-            p.push(download(src, `../鬼灭之刃/${title}`, {
+            p.push(download(src, `../一人之下/${title}`, {
               filename: `${i}.jpg`.toString()
-            }).then(() => {console.log(`鬼灭之刃/${title}`, '下载完成')}).catch(e=>{}))
+            }).then(() => {console.log(`一人之下/${title}`, '下载完成')}).catch(e=>{}))
             i++;
           }
 
@@ -34,26 +84,26 @@ module.exports = (app) => {
           console.log(url)
         })
       }
-      console.log('鬼灭之刃:全部完成')
+      console.log('一人之下:全部完成')
       ctx.body = ''
     },
 
     async other() {
       const { ctx } = this;
-      let i = 31;
-      while(i <= 182){
+      let i = 8001;
+      while(i <= 8002){
         let j = 1;
-        const url = `https://twocomic.com/view/comic_10406.html?ch=${i}-${j}`;
+        const url = `https://twocomic.com/view/comic_7340.html?ch=${i}-${j}`;
         const {page} = u(j, url);
         const p = [];
         console.log('共',page, '页')
         while(j <= page){
-          const url = `https://twocomic.com/view/comic_10406.html?ch=${i}-${j}`;
+          const url = `https://twocomic.com/view/comic_7340.html?ch=${i}-${j}`;
           const {img} = u(j, url);
           let page = j;
-          p.push(download(img, `../一拳超人/第${i}话`, {
+          p.push(download(img, `../进击的巨人/第${i}话`, {
             filename: `${j}.jpg`.toString()
-          }).then(() => {console.log(`一拳超人/第${i}话-第${page}页`, '下载完成')}).catch(e=>{}))
+          }).then(() => {console.log(`进击的巨人/第${i}话-第${page}页`, '下载完成')}).catch(e=>{}))
           j++;
         }
 
