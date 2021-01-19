0617efd3abdffb986fb64355a5a5a07c1031dcf4
JJ <303907720@qq.com>
2021-01-20 00:49:12 +0800

    add

--- app/service/main.js
+++ app/service/main.js
@@ -1,5 +1,6 @@
 const ProgressBar = require('progress');
 const axios = require('axios');
+const download = require('download');
 const chunk = require('lodash/chunk');
 const fs = require('fs');
 
@@ -10,33 +11,46 @@ module.exports = (app) => {
       const { ctx } = this;
       const bar = new ProgressBar(`${title}(共${total}页): [:bar] [:percent]`, { total: +total, width: 50, clear: true });
       const newArr = chunk(arr, max)
-      for(const list of newArr){
-        const p = list.map(({url, path, page}) => {
-          url = url.indexOf('http') === -1 ? `http:${url}` : url;
-          return  axios({
-              url,
-              method: 'get',
-              responseType: "arraybuffer",
-              timeout: 1000,
-              headers: {
-                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
-                'cache-control': 'no-cache',
-                accept: 'image/webp,image/apng,image/*,*/*;q=0.8'
-              }
-            }).then(res => {
-              if(!fs.existsSync(path)){
-                fs.mkdirSync(path)
-              }
-              fs.writeFileSync(`${path}${page}.jpg`, res.data, 'binary')
-              bar.tick(1);  //进度步长
-         
-            }).catch(e => {
-              console.log(e.message)
-            })
-          })
-        await Promise.all(p)
-      }
+      return new Promise(async (res) => {
+        for(const list of newArr){
+          await Promise.all(list.map(({url, path, page}) => {
+            download(url, `${path}`, {filename: `${page}.jpg`}).then(() => {bar.tick(1);})
+          }))
+        }
+        res({})
+      })
+     
       
+      // await Promise.all();
+      
+
+      //   const p = list.map(({url, path, page}) => {
+      //     url = url.indexOf('http') === -1 ? `http:${url}` : url;
+
+         
+          // return axios({
+          //     url,
+          //     method: 'get',
+          //     responseType: "arraybuffer",
+          //     timeout: 1000,
+          //     headers: {
+          //       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+          //       'cache-control': 'no-cache',
+          //       accept: 'image/webp,image/apng,image/*,*/*;q=0.8'
+          //     }
+          //   }).then(res => {
+          //     if(!fs.existsSync(path)){
+          //       fs.mkdirSync(path)
+          //     }
+          //     fs.writeFileSync(`${path}${page}.jpg`, res.data, 'binary')
+          //     bar.tick(1);  //进度步长
+         
+          //   }).catch(e => {
+          //     console.log(e.message)
+          //   })
+        //   })
+        // await Promise.all(p)
     }
+      
   };
 };
