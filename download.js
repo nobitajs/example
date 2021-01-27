767e309bf202c2a9541f3cb5f1302bac8c762859
JJ <303907720@qq.com>
2021-01-27 10:37:44 +0800

    add

--- download.js
+++ download.js
@@ -2,66 +2,77 @@ const ProgressBar = require('progress');
 const fs = require('fs');
 const fetch = require('node-fetch');
 
-async function newDownload(images, options = {}){
-    const { title = '名称', parallel = 5, timeout = 5000 } = options;
+let bar = null;
+async function download(images, options = {}){
+    const { title = '名称', parallel = 5 } = options;
     const total = images.length;
-    const bar = new ProgressBar(`${title}(:current/:total): [:bar] [:percent]`, { total: +total, width: 50, clear: true });
+    bar = new ProgressBar(`${title}(:current/:total): [:bar] [:percent]`, { total: +total, width: 50 });
     let queue = parallel;
     let finish = null;
-    function callback(){
+    
+    function callback(allPath, n){
         bar.tick(1);
         const image = images.shift();
+        // console.log(allPath, n, '============');
+        // bar.interrupt(`${allPath} 下载完成`);
+
         queue--;
         if(image){
             queue++
-            downloadFile(image, timeout, callback);
+            downloadFile(image, options, callback);
         }
 
-        // console.log(images, queue, '===queue====')
+        // bar.tick({text: images, queue, '===queue====')
         if(queue <= 0){
+            bar.interrupt(`${title} 下载完成`);
             finish(`${title}下载完成`);
         }
-        
     }
 
     return new Promise((resolve) => {
         for(let i = 0; i < parallel; i++){
             const image = images.shift();
-            image && downloadFile(image, timeout, callback);
+            image && downloadFile(image, options, callback);
         }
         finish = resolve
     })
 }
 
-function downloadFile(image, timeout, callback){
+function downloadFile(image = {}, options = {}, callback = () => {}){
+    const { timeout = 5000, gainInterval = 3000, againTimes = 0 } = options;
     let { url, path = '/', fileName = createFileName(), extract = 'jpg' } = image;
-    if(!url) return callback();
+    const allPath = `${path}${fileName}.${extract}`;
+    if(!url) return callback(allPath, 2);
     url = url.indexOf('http') === -1 ? `http:${url}` : url;
     let timer = null;
     if(!fs.existsSync(path)){
         fs.mkdirSync(path, {recursive: true})
     }
 
-    let writeStream = fs.createWriteStream(`${path}${fileName}.${extract}`);
+    const writeStream = fs.createWriteStream(allPath);
     const timeoutCallback = () => { 
         writeStream.destroy('timeout')
     }
 
-    let failTimeout = null;
+    let tryAgain = null;
     const failCallBack = () => { 
-        downloadFile(image, timeout, callback) 
+        image.againTimes = image.againTimes ? image.againTimes + 1 : 1;
+        if(againTimes === 0 || image.againTimes <= againTimes){
+            downloadFile(image, options, callback) 
+        }else{
+            bar.interrupt(`${image.url}, 超过重试次数！`);
+        }
     };
 
-
     writeStream
-        .on('close', () => {
+        .on('finish', () => {
             clearTimeout(timer);
-            callback();
+            callback(allPath, 1);
         })
         .on('error', () => {
-            console.log(`${path}${fileName}.${extract} 下载错误`)
-            clearTimeout(failTimeout);
-            failTimeout = setTimeout(failCallBack, timeout);
+            bar ? bar.interrupt(`${allPath} 下载错误`) : console.log(`${allPath} 下载错误`);
+            clearTimeout(tryAgain);
+            tryAgain = setTimeout(failCallBack, gainInterval);
         })
         .on('pipe', () => {
             clearTimeout(timer);
@@ -72,85 +83,17 @@ function downloadFile(image, timeout, callback){
             timer = setTimeout(timeoutCallback, timeout);
         })
 
-    fetch(url, {timeout: timeout + 1000})
+    fetch(url, {timeout: timeout + 300})
         .then(res => res.body.pipe(writeStream))
         .catch((err) => {
-            console.log(`${path}${fileName}.${extract} 请求超时`)
-            clearTimeout(failTimeout);
-            failTimeout = setTimeout(failCallBack, timeout);
+            bar ? bar.interrupt(`${allPath} 请求超时`) : console.log(`${allPath} 请求超时`);
+            clearTimeout(tryAgain);
+            tryAgain = setTimeout(failCallBack, gainInterval);
         })
-}
-
-async function download(arr, options = {}) {
-    const { title = '名称', parallel = 5, timeout = 5000 } = options;
-    const total = arr.length;
-    const bar = new ProgressBar(`${title}(:current/:total): [:bar] [:percent]`, { total: +total, width: 50, clear: true });
-    let result = {
-        successList: [],
-        failList: [],
-    };
-    while(arr.length > 0){
-        const getImages = arr.splice(0, parallel);
-        let p = [];
-
-        for(image of getImages){
-            let { url, path = '/', fileName = createFileName(), extract = 'jpg' } = image;
-            if(!url) continue;
-            url = url.indexOf('http') === -1 ? `http:${url}` : url;
-            p.push(new Promise((resolve, reject) => {
-                let timer = null;
-                if(!fs.existsSync(path)){
-                    fs.mkdirSync(path, {recursive: true})
-                }
-
-                let writeStream = fs.createWriteStream(`${path}${fileName}.${extract}`);
-                const timeoutCallback = () => { 
-                    writeStream.destroy('timeout')
-                }
-
-                writeStream
-                    .on('close', () => {
-                        bar.tick(1);
-                        clearTimeout(timer);
-                        resolve({status: true, image});
-                    })
-                    .on('error', () => {
-                        console.log(`${path}${fileName}.${extract} 下载错误`)
-                        arr.push({url, path, fileName})
-                        resolve({status: false, image});
-                    })
-                    .on('pipe', () => {
-                        clearTimeout(timer);
-                        timer = setTimeout(timeoutCallback, timeout);
-                    })
-                    .on('drain', () => {
-                        clearTimeout(timer);
-                        timer = setTimeout(timeoutCallback, timeout);
-                    })
-                    fetch(url, {timeout: timeout + 1000})
-                    .then(res => res.body.pipe(writeStream))
-                    .catch((err) => {
-                        console.log(`${path}${fileName}.${extract} 请求超时`)
-                        arr.push({url, path, fileName})
-                        resolve({status: false, image});
-                    })
-            }))
-        }
-
-        const results = await Promise.all(p).catch(e => {
-            console.log(e, `===========下载失败==========`)
-        })
-
-        results.forEach(({status, image}) => {
-            status ? result.successList.push(image) : result.failList.push(image);
-        })
-    }
-
-    return result
 }
 
 function createFileName(){
     return `${Date.now()}${Math.ceil(Math.random() * 5000 + 5000)}`;
 }
 
-module.exports = newDownload;
+module.exports = download;
