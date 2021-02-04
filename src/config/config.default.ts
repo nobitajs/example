8f904536d301d84b08607860b585c439675c0b1f
JJ <tangjun@zuzuche.com>
2021-02-04 16:46:49 +0800

    add

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -1,21 +1,16 @@
 const config = {
 	
-	redis: {
-		port: 6379,          // Redis port
-		host: '127.0.0.1',   // Redis host
-		family: 4,           // 4 (IPv4) or 6 (IPv6)
-		db: 0
-	},
+	// redis: {
+	// 	port: 6379,          // Redis port
+	// 	host: '127.0.0.1',   // Redis host
+	// 	family: 4,           // 4 (IPv4) or 6 (IPv6)
+	// 	db: 0
+	// },
 
-	mongo: {
-		url: 'mongodb://127.0.0.1:27017/app_push',
-	},
+	// mongo: {
+	// 	url: 'mongodb://127.0.0.1:27017/app_push',
+	// },
 
-	push: {
-		key: 'admin',
-		secret: 'bnUPx6OyMBVcdca7l9U6uoNJnIX2QgPm',
-	},
-	appushHost: 'https://appush.zuzuche.net',
 
 };
 export default config;
