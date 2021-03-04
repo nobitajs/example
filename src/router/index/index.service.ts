40890edfffa0651d5772e14472a8f295e2c12d3f
JJ <tangjun@zuzuche.com>
2021-03-04 15:23:53 +0800

    add

--- src/router/index/index.service.ts
+++ src/router/index/index.service.ts
@@ -8,8 +8,7 @@ import { HelperService } from '../../common/providers/helper/helper.service';
 import { IndexDataDto, IndexGetSizeIdDataDto } from './index.dto';
 
 let time = 0;
-let sizeId = ['1635916714748932' , '1635910918152321' , '1635919428929111' , '1635919428937303' , '1635919428908631' , '1635916714724356','1635916714736644', '1635910918140033']
-// let sizeId = ['1617219403782153', '1482044468991493', '1596649056887821', '1627099494265932', '1627105556379541', '1627105555658645', '1627099494552652']
+let sizeId = ['1667816005403977', '1667799596280896']
 @Injectable()
 export class IndexService {
 
@@ -23,26 +22,10 @@ export class IndexService {
 	}
 	async buy(data: IndexDataDto){
 		let i = 0;
-		let max = 2;
+		let max = 1;
 		let n = 0;
 		while(true){
-			let p = [axios({
-				url: 'https://www.huahaicang.cn/api/neptune/neptune/cart/add/v2',
-				method: 'post',
-				data: qs.stringify({
-					sizeNum: '1',
-					source: 'huahaicang_iphone',
-					sizeId: '1635824717765598',
-					method: 'POST',
-					timestamp: (+new Date() / 1000 - i).toFixed(),
-				}),
-				headers: {
-					'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
-					'Content-Type': 'application/x-www-form-urlencoded',
-					referer: 'https://www.huahaicang.cn/',
-					cookie: 'hhc-param=bda37924cee6014e70dfb46a79905931a63692cf; mars_cid=1609900371316_2016e3181ff0ca1a428ed470f9ca72d9; areaId=944101103999; WAP[p_wh]=VIP_NH; warehouse=VIP_NH; m_vip_province=104104101103; WAP[p_area]=%25E5%25B9%25BF%25E4%25B8%259C%25E7%259C%2581.%25E5%25B9%25BF%25E5%25B7%259E%25E5%25B8%2582.%25E8%258D%2594%25E6%25B9%25BE%25E5%258C%25BA; saturn=vst4301l4j38tjhbg8ksnp9ql10; triton=CCC8C0CE745C8596F8AEAE63C732638C6F2E249C; _t_offset=0; _t_=1611884212',
-				}
-			})];
+			let p = [];
 			while(p.length < max){
 				if(i >= sizeId.length){
 					i = 0;
@@ -69,14 +52,14 @@ export class IndexService {
 						'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
 						'Content-Type': 'application/x-www-form-urlencoded',
 						referer: 'https://www.huahaicang.cn/',
-						cookie: 'hhc-param=bda37924cee6014e70dfb46a79905931a63692cf; mars_cid=1609900371316_2016e3181ff0ca1a428ed470f9ca72d9; areaId=944101103999; WAP[p_wh]=VIP_NH; warehouse=VIP_NH; m_vip_province=104104101103; WAP[p_area]=%25E5%25B9%25BF%25E4%25B8%259C%25E7%259C%2581.%25E5%25B9%25BF%25E5%25B7%259E%25E5%25B8%2582.%25E8%258D%2594%25E6%25B9%25BE%25E5%258C%25BA; saturn=vst4301l4j38tjhbg8ksnp9ql10; triton=CCC8C0CE745C8596F8AEAE63C732638C6F2E249C; _t_offset=0; _t_=1611884212',
+						cookie: 'hhc-param=bda37924cee6014e70dfb46a79905931a63692cf; mars_cid=1609900371316_2016e3181ff0ca1a428ed470f9ca72d9; areaId=944101103101; WAP[p_wh]=VIP_NH; warehouse=VIP_NH; m_vip_province=104104101103; WAP[p_area]=%25E5%25B9%25BF%25E4%25B8%259C%25E7%259C%2581.%25E5%25B9%25BF%25E5%25B7%259E%25E5%25B8%2582.%25E8%258D%2594%25E6%25B9%25BE%25E5%258C%25BA; _t_offset=0; saturn=v4gkccm692n8isst7l488nbr693; triton=CCC8C0CE745C8596F8AEAE63C732638C6F2E249C; _t_=1614309600',
 					}
 				}));
 				i++;
 			}
 			n += max;
 			if(n % 16 === 0){
-				time = 2100;
+				time = 2200;
 			}else{
 				time = 0
 			}
@@ -88,7 +71,7 @@ export class IndexService {
 			})
 			const res = await Promise.all(p)
 			res.forEach(data=>{
-				console.log(data.data.msg, data.config.data);
+				console.log(data.data.msg);
 			})
 			
 		}
