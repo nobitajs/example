fb27fbd82ee3172a59b3ed49d557ef18906a9332
JJ <tangjun@zuzuche.com>
2021-05-20 18:09:28 +0800

    add

--- src/common/providers/kafka/kafka.providers.ts
+++ src/common/providers/kafka/kafka.providers.ts
@@ -1,10 +1,16 @@
+import { ConfigService } from '../config/config.service';
+
 export const KafkaConnectionProviders = {
 	provide: 'KAFKA_CONNECTION',
-	useFactory: (): any => {
+	useFactory: (config): any => {
 		const Kafka = require('kafka-node');
+		const kafkaConfig = config.get('kafka');
 		const Producer = Kafka.Producer;
-		const client = new Kafka.KafkaClient({ kafkaHost: 'kafka1.bigdata.zuzuche.info:9092,kafka2.bigdata.zuzuche.info:9092,kafka3.bigdata.zuzuche.info:9092' });
+
+		if(!kafkaConfig) return 
+		const client = new Kafka.KafkaClient({ kafkaHost: kafkaConfig.host });
 		const producer = new Producer(client);
 		return producer;
 	},
+	inject: [ConfigService]
 };
---
+++ src/config/config.dev.ts
@@ -0,0 +1,8 @@
+export default {
+	redis: {
+		port: 63379,
+		host: 'redis.rocketos-middleware-dev.svc.rocketos.local',
+		password: 'DNtoxikc8JZM',
+		db: 4,
+	},
+};
