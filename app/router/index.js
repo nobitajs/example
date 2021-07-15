5110ee63ea2cb239b2361c1d9f8680a6f5ba6e23
JJ <303907720@qq.com>
2021-07-15 15:34:06 +0800

    add

--- app/router/index.js
+++
@@ -1,6 +0,0 @@
-
-module.exports = app => {
-  app.router.get('/',{}, app.controllers.index.index);
-  app.router.get('/other',{}, app.controllers.index.other);
-  app.router.get('/a',{}, app.controllers.index.a);
-};
